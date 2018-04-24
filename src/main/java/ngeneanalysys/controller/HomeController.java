b694cd358a208ee81dd01ed78456bf5e6ff0e6e5
hongiiv <hongiiv@gmail.com>
2018-04-24 22:21:21 +0900

    홈(대시보드) 화면 너비 변경

--- src/main/java/ngeneanalysys/controller/HomeController.java
+++ src/main/java/ngeneanalysys/controller/HomeController.java
@@ -150,15 +150,15 @@ public class HomeController extends SubPaneController{
     }
 
     private void initRunListLayout() {
-        final int maxRunNumberOfPage = 3;
+        final int maxRunNumberOfPage = 4;
         try {
             runList = new ArrayList<>();
             for (int i = 0; i < maxRunNumberOfPage; i++) {
                 RunStatusVBox box = new RunStatusVBox();
                 runList.add(box);
-                runListHBox.setPrefWidth(runListHBox.getPrefWidth() + 247);
+                runListHBox.setPrefWidth(runListHBox.getPrefWidth() + 235);
                 runListHBox.getChildren().add(box);
-                runListHBox.setSpacing(37);
+                runListHBox.setSpacing(10);
             }
         } catch (Exception e) {
             logger.error("HOME -> initRunListLayout", e);
