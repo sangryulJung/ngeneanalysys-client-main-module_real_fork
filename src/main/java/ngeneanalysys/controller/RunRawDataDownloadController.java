e39e02ceacab090a65d224ffeea783d0015eb79f
JoonsikJang-NGeneBio <js.jang@ngenebio.com>
2018-11-08 13:43:28 +0900

    Run별 분석 결과 파일 다운로드 화면 변경

--- src/main/java/ngeneanalysys/controller/RunRawDataDownloadController.java
+++ src/main/java/ngeneanalysys/controller/RunRawDataDownloadController.java
@@ -1,23 +1,27 @@
 package ngeneanalysys.controller;
 
-import javafx.collections.ObservableList;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.*;
+import org.controlsfx.tools.Borders;
+import org.slf4j.Logger;
 import ngeneanalysys.code.constants.CommonConstants;
 import ngeneanalysys.controller.extend.SubPaneController;
 import ngeneanalysys.model.RunSampleView;
 import ngeneanalysys.util.DialogUtil;
 import ngeneanalysys.util.LoggerUtil;
-import org.controlsfx.control.CheckComboBox;
-import org.slf4j.Logger;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Jang
@@ -33,7 +37,7 @@ public class RunRawDataDownloadController extends SubPaneController {
     @FXML
     private HBox hBox;
 
-    private CheckComboBox<String> checkComboBox;
+    private List<CheckBox> checkBoxes;
 
     /**
      * @param runSampleView RunSampleView
@@ -66,19 +70,38 @@ public class RunRawDataDownloadController extends SubPaneController {
     }
 
     private void createCheckComboBox() {
-        checkComboBox = new CheckComboBox<>();
-        checkComboBox.getItems().addAll("bai", "bam", "vcf", "fastq.gz");
-        hBox.getChildren().add(checkComboBox);
+        checkBoxes = new ArrayList<>();
+        checkBoxes.add(new CheckBox("bai"));
+        checkBoxes.add(new CheckBox("bam"));
+        checkBoxes.add(new CheckBox("vcf"));
+        checkBoxes.add(new CheckBox("fastq,gz"));
+        GridPane grid = new GridPane();
+        grid.setVgap(10.0);
+        grid.setHgap(10.0);
+        grid.getRowConstraints().add(new RowConstraints(30));
+        grid.getRowConstraints().add(new RowConstraints(30));
+        grid.getColumnConstraints().add(new ColumnConstraints(100));
+        grid.getColumnConstraints().add(new ColumnConstraints(100));
+        grid.add(checkBoxes.get(0), 0, 0);
+        grid.add(checkBoxes.get(1), 0, 1);
+        grid.add(checkBoxes.get(2), 1, 0);
+        grid.add(checkBoxes.get(3), 1, 1);
+        grid.setPrefSize(210, 70);
+        Node wrappedCheckBox = Borders.wrap(grid)
+                .lineBorder().title("File Types").thickness(1).radius(0, 5, 5, 0).build()
+                .emptyBorder().padding(10).buildAll();
+        hBox.getChildren().add(wrappedCheckBox);
     }
 
     @FXML
     public void rawDataDownload() {
-        ObservableList<String> checkItem = checkComboBox.getCheckModel().getCheckedItems();
-        if(!checkItem.isEmpty()) {
-            List<String> list = new ArrayList<>(checkItem);
-            downloadTask(list);
+        List<String> selectedFileTypes = checkBoxes.stream().filter(CheckBox::isSelected).map(CheckBox::getText)
+                .collect(Collectors.toList());
+
+        if(!selectedFileTypes.isEmpty()) {
+            downloadTask(selectedFileTypes);
         } else {
-            DialogUtil.warning("", "Please select file to delete.", mainController.getPrimaryStage(), true);
+            DialogUtil.warning("", "Please select file types to download.", mainController.getPrimaryStage(), true);
         }
     }
 
